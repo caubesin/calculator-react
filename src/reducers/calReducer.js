801bffac7a151ab7fe61662441f361c1c44e4d65
caubesin <60962992+caubesin@users.noreply.github.com>
2021-04-17 21:13:21 +0700

    fix bug cal

--- src/reducers/calReducer.js
+++ src/reducers/calReducer.js
@@ -10,9 +10,6 @@ const calReducer = (state = initialState, action) => {
     
     switch (action.type) {
         case ADD_SYMBOL:
-            if(state.val === "0") {
-                state.val = state.val.substr(1);
-            }
             if(state.isEval === true) {
                 state.val = action.val;
                 state.isEval = false;
@@ -26,14 +23,22 @@ const calReducer = (state = initialState, action) => {
                     ...state,
                 }
             }
+            if(state.val[state.val.length-1]==="0" && reg.test(state.val[state.val.length-2])) {
+                return state = {
+                    ...state,
+                    val : state.val.replace(state.val[state.val.length-1],"") + action.val
+                }
+            }
+            if(state.val[0] === "0" && action.val === "0") {
+                return state = {
+                    ...state,
+                }
+            }
             return state = {
                 ...state,
                 val : state.val + action.val,
             }
         case EVEN:
-            if(state.val[0] === "0") {
-                state.val = state.val.substr(1);
-            }
             try {
                 eval(state.val)
             }
@@ -50,9 +55,12 @@ const calReducer = (state = initialState, action) => {
                 isEval : true
             }
         case DELETE_SYMBOL:
+            if(state.val === "0") {
+                state.val = state.val.substr(1);
+            }
             return state = {
                 ...state,
-                val : state.val.length === 1 ? "0" : state.val.substr(0, state.val.length - 1),
+                val : state.val.substr(0, state.val.length - 1),
             }
         case CLEAR_SYMBOL:
             return state = {
